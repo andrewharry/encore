0c50ddda9403ad7f620eb02031b40e27ba84c7ac
Andrew Harry <ajharry@gmail.com>
2022-05-30 10:35:50 +0930

    Minor renaming

--- Encore.Testing/TestWithEFCore.cs
+++ Encore.Testing/TestWithEFCore.cs
@@ -1,5 +1,4 @@
 ﻿using Encore.Testing.Services;
-using Encore.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -7,28 +6,27 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Encore.Testing
 {
-    public abstract class TestWithEFCore : TestWithLogs
+    public abstract class TestWithEfCore : TestWithLogs
     {
         [NotNull]
-        private DataAccessHelper? dataAccess { get; set; }
+        private DataAccessHelper? DataAccess { get; set; }
 
         [NotNull]
-        private DbContextResolver? dbContextResolver { get; set; }
+        private DbContextResolver? DbContextResolver { get; set; }
 
         /// <summary>
         /// Creates a InMemory Test DbContext.  Needs to be called prior to SetupResolver
         /// </summary>
         protected virtual void CreateDatabase<TDbContext>() where TDbContext : DbContext
         {
-            if (dataAccess != null)
+            if (DataAccess != null)
                 throw new NullReferenceException($"{nameof(CreateDatabase)} needs to be called before {nameof(SetupResolver)}");
 
-            if (dbContextResolver == null)
-                dbContextResolver = new DbContextResolver();
+            DbContextResolver = DbContextResolver ?? new DbContextResolver();
 
             var type = typeof(TDbContext);
             var databaseName = type.Name;
-            dbContextResolver.Add(type);
+            DbContextResolver.Add(type);
 
             Registry.Register(type, ServiceLifetime.Transient);
             Registry.ServiceCollection.AddDbContextFactory<TDbContext>(option => SetOptions(databaseName, option), ServiceLifetime.Singleton);
@@ -37,10 +35,10 @@ namespace Encore.Testing
 
         protected override void SetupResolver()
         {
-            if (dataAccess == null) { 
+            if (DataAccess == null) { 
                 base.SetupResolver();
-                dbContextResolver.SetupResolver(Resolver);
-                dataAccess = new DataAccessHelper(Resolver, dbContextResolver);
+                DbContextResolver.SetupResolver(Resolver);
+                DataAccess = new DataAccessHelper(Resolver, DbContextResolver);
             }
         }
 
@@ -55,27 +53,27 @@ namespace Encore.Testing
         protected override void OnPreCleanup()
         {
             base.OnPreCleanup();
-            dbContextResolver?.Dispose();
+            DbContextResolver?.Dispose();
         }
 
         protected void SetItems<TEntity>(IEnumerable<TEntity> items) where TEntity : class
         {
-            dataAccess.SetItems(items.ToSafeArray());
+            DataAccess.SetItems(items.ToSafeArray());
         }
 
         protected void SetItems<TEntity>(params TEntity[] items) where TEntity : class
         {
-            dataAccess.SetItems(items);
+            DataAccess.SetItems(items);
         }
 
         protected IEnumerable<TEntity> GetItems<TEntity>(Func<TEntity, bool> where) where TEntity : class
         {
-            return dataAccess.GetItems(where);
+            return DataAccess.GetItems(where);
         }
 
         protected TEntity? FirstOrDefault<TEntity>() where TEntity : class
         {
-            return dataAccess.FirstOrDefault<TEntity>(v => true);
+            return DataAccess.FirstOrDefault<TEntity>(v => true);
         }
     }
 }
